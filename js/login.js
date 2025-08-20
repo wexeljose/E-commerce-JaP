0cf2c549db97d48307deeb20b1b196d9ee2d75cc
wexeljose <wexeljose@gmail.com>
2025-08-20 15:27:30 -0300

    Se arregló un error introducido en el login.

--- js/login.js
+++ js/login.js
@@ -7,6 +7,8 @@ function procesarLogin() {
       document.getElementById("usuario").value.trim()
     );
     moverAIndex();
+  } else {
+    alert("Por favor, complete todos los campos.");
   }
 }
 
@@ -15,5 +17,16 @@ function moverAIndex() {
 }
 
 function validacion() {
-  return true;
+  let campoUsuario = document.getElementById("usuario");
+  let campoContrasena = document.getElementById("password");
+
+  if (!campoUsuario || !campoContrasena) {
+    console.error("No se encontraron los inputs en el DOM.");
+    return false;
+  }
+
+  let usuario = campoUsuario.value.trim();
+  let contrasena = campoContrasena.value.trim();
+
+  return usuario !== "" && contrasena !== "";
 }
