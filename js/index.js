f957725766311bb6634767b448893459fe9bfbc0
wexeljose <wexeljose@gmail.com>
2025-08-29 14:57:01 -0300

    Feature: Se agregó el nombre de usuario al index.html de forma responsiva en el nav

--- js/index.js
+++ js/index.js
@@ -1,14 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("autos").addEventListener("click", function () {
+    localStorage.setItem("catID", 101);
+    window.location = "products.html";
+  });
+  document.getElementById("juguetes").addEventListener("click", function () {
+    localStorage.setItem("catID", 102);
+    window.location = "products.html";
+  });
+  document.getElementById("muebles").addEventListener("click", function () {
+    localStorage.setItem("catID", 103);
+    window.location = "products.html";
+  });
 });
+
+const user = localStorage.getItem("usuario");
+
+if (user) {
+  console.log("Usuario encontrado");
+  const userName = user;
+
+  const userAvatar = document.getElementById("user-avatar");
+  const usernameSpan = document.getElementById("username");
+
+  usernameSpan.textContent = userName;
+
+  const encodedName = encodeURIComponent(userName);
+  const apiUrl = `https://ui-avatars.com/api/?name=${encodedName}&background=random&size=30`;
+
+  userAvatar.src = apiUrl;
+}
